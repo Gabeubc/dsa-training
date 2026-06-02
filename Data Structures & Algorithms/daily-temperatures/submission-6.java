b0adf6a815311a10542a67a438c255b428d2c20b
Gabriel Youbissi <youbigabriel@gmail.com>
2026-06-02 11:49:20 +0200

    Add: daily-temperatures - submission-6

---
+++ Data Structures & Algorithms/daily-temperatures/submission-6.java
@@ -0,0 +1,20 @@
+class Solution {
+    record Temperature(
+        int val,
+        Integer pos
+    ){}
+    public int[] dailyTemperatures(int[] temperatures) {
+        Deque<Temperature> stack=new ArrayDeque<>();
+        int len=temperatures.length;
+        int[] res=new int[len];
+        stack.push(new Temperature(temperatures[0], 0));
+        for(int i=1; i<len; i++){
+            while(stack.peek()!=null && stack.peek().val<temperatures[i]){
+                Temperature t=stack.pop();
+                res[t.pos]=i-t.pos;
+            }
+            stack.push(new Temperature(temperatures[i], i));
+        }
+        return res;
+    }
+}
